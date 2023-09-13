8e769e4ddb95b1dc20e21023dd8f47550887605f
Diósney Gomes <diosney.gomes@beeit.com.br>
2023-09-13 13:20:39 -0300

    Cria classes de mapeamento das propriedades das tabelas do banco

---
+++ src/Impar.BackEnd.Evaluation.Data/EntityMappingConfigurations/MessageConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Impar.BackEnd.Evaluation.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Impar.BackEnd.Evaluation.Data.EntityMappingConfigurations
+{
+    public class MessageConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder
+                .HasKey(message => message.Id);
+
+            builder
+                .Property(message => message.Subject)
+                .IsRequired();
+
+            builder
+                .Property(message => message.MessageContent)
+                .IsRequired();
+
+            builder
+                .Property(message => message.SentAt)
+                .IsRequired();
+        }
+    }
+}
---
+++ src/Impar.BackEnd.Evaluation.Data/EntityMappingConfigurations/UserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Impar.BackEnd.Evaluation.Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Impar.BackEnd.Evaluation.Data.EntityMappingConfigurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .HasKey(user => user.Id);
+
+            builder
+                .Property(user => user.Name)
+                .IsRequired();
+
+            builder
+                .Property(user => user.Email)
+                .IsRequired();
+
+            builder
+                .Property(user => user.Phone)
+                .IsRequired();
+
+            builder
+                .HasMany(messager => messager.Messages)
+                  .WithOne(user => user.User);
+        }
+    }
+}
